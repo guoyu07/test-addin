d5fa4e888aced6d891591aeecf31427353e88819
Rafael Steil <rafael@insanecorp.com>
2013-08-08 19:47:17 -0300

    created the project

---
+++ TestHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoDevelop.Components.Commands;
+
+namespace TestAddin
+{
+	public class TestHandler : CommandHandler
+	{
+		protected override void Run() {
+			
+		}
+	}
+}
